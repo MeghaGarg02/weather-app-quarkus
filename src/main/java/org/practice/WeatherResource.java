05c4812b80d6e3aea342d39f98fa97b237a72adc
Megha_Garg <Megha_Garg@192.168.18.102>
2024-04-12 14:17:27 +0200

    Adding Exception Handling

--- src/main/java/org/practice/WeatherResource.java
+++ src/main/java/org/practice/WeatherResource.java
@@ -1,14 +1,14 @@
 package org.practice;
 
-import java.io.IOException;
 import java.util.Optional;
+
+import org.practice.exception.ServiceException;
 
 import io.quarkus.logging.Log;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
-import jakarta.ws.rs.core.UriBuilderException;
 
 @Path("/weather")
 public class WeatherResource {
@@ -17,9 +17,13 @@ public class WeatherResource {
 private AppConfig appConfig;
 
     @GET()
-    public Optional<WeatherResponse> hello(@QueryParam(value = "cityName") String cityName) throws IllegalArgumentException, UriBuilderException, IOException {
-    Log.info("Passing the request for getting the Weather Data for City: " + cityName);
-    //cityName = "Stavanger";
-    return appConfig.getWeatherService().findByCityName(cityName);
+    public Optional<WeatherResponse> hello(@QueryParam(value = "cityName") String cityName) {
+        Log.info("Passing the request for getting the Weather Data for City: " + cityName);
+        Optional<WeatherResponse> response = appConfig.getWeatherService().findByCityName(cityName);
+        if(response.isPresent()) {
+            return response;
+        } else {
+            throw new ServiceException("Resource Not Found");
+        }
     }
 }
--- src/main/java/org/practice/WeatherService.java
+++ src/main/java/org/practice/WeatherService.java
@@ -5,8 +5,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Optional;
 
+import org.practice.exception.ServiceException;
 import org.practice.weather.map.response.OpenWeatherMapResponseData;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.quarkus.logging.Log;
@@ -21,11 +24,28 @@ public class WeatherService {
     @Inject
     private AppConfig appConfig;
 
-    public Optional<WeatherResponse> findByCityName(String cityName) throws IllegalArgumentException, UriBuilderException, IOException {
+    public Optional<WeatherResponse> findByCityName(String cityName){
       Log.info("Reading Weather Json data from https://openweathermap.org/ by city and appId---");
-      OpenWeatherMapResponseData data =  new ObjectMapper().readValue(this.getOpenWeatherMapURLByCityName(cityName), OpenWeatherMapResponseData.class);
-      Log.info("Weather Map Data String JSON is " +  data.toString());
-      return getWeatherResponse(data, cityName);
+      OpenWeatherMapResponseData data;
+        try {
+            if(this.getOpenWeatherMapURLByCityName(cityName) != null) {
+                data = new ObjectMapper().readValue(this.getOpenWeatherMapURLByCityName(cityName), OpenWeatherMapResponseData.class);
+                if(data != null) {
+                    Log.info("Weather Map Data String JSON is " +  data.toString());
+                    return getWeatherResponse(data, cityName);
+                }
+            }
+        } catch (StreamReadException e) {
+            Log.error("Error in reading Open weather api response stream---", e);
+        } catch (DatabindException e) {
+            Log.error("Error in converting Open weather api response to JSON object---", e);
+        } catch (IOException e) {
+            Log.error("IO error occurred while calling the Open weather api response---", e);
+        } catch (Exception e) {
+            Log.errorf("Service Exception Occured while fetching the open weather api response---", e);
+            throw new ServiceException("Service Exception Occured while building the open weather api URL for city and appId ---");
+        }
+      return Optional.ofNullable(null);
     }
 
     private Optional<WeatherResponse> getWeatherResponse(OpenWeatherMapResponseData data, String cityName) {
@@ -34,14 +54,23 @@ public class WeatherService {
         return Optional.of(new WeatherResponse(message));
     }
 
-    private URL getOpenWeatherMapURLByCityName(String cityName) throws MalformedURLException, IllegalArgumentException, UriBuilderException {
-        return UriBuilder.newInstance()
-                .scheme(appConfig.getProperties().getScheme())
-                .host(appConfig.getProperties().getHostName())
-                .path(appConfig.getProperties().getPathValue())
-                .queryParam("q", cityName)
-                .queryParam("appid", appConfig.getProperties().getAppKey())
-                .build()
-                .toURL();
+    private URL getOpenWeatherMapURLByCityName(String cityName) {
+        URL url = null;
+        try {
+            url = UriBuilder.newInstance()
+                    .scheme(appConfig.getProperties().getScheme())
+                    .host(appConfig.getProperties().getHostName())
+                    .path(appConfig.getProperties().getPathValue())
+                    .queryParam("q", cityName)
+                    .queryParam("appid", appConfig.getProperties().getAppKey())
+                    .build()
+                    .toURL();
+        } catch (MalformedURLException | IllegalArgumentException | UriBuilderException e) {
+            Log.error("Error in generating the Open weather api URL ---", e);
+        } catch (Exception e) {
+            Log.errorf("Service Exception Occured while building the open weather api URL for city and appId ---", e);
+            throw new ServiceException("Service Exception Occured while building the open weather api URL for city and appId ---");
+        }
+        return url;
     }
 }
